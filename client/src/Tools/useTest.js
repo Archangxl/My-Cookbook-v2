07b4387a17a0bc090e0ade13bc232b1b6928fa2c
Alexander <lixo.mueller@gmail.com>
2023-06-25 17:28:55 -0600

    last updated 06.25.2023

--- client/src/Tools/useTest.js
+++ client/src/Tools/useTest.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 const usePostRecipe = () => {
 
     const [data, setData] = useState(null);
-    const [error, setError] = useState(null);
+    const [error, setError] = useState({recipeName: null, ingredient: null, instructions: null});
     const [urlForPost, setUrlForPost] = useState(null);
     const [recipeObject, setRecipeObject] = useState({});
 
@@ -17,9 +17,41 @@ const usePostRecipe = () => {
                 {withCredentials: true}
             )
                 .then(res => setData(res))
-                .catch(err => setError(err));
+                .catch(err => {
+                    checkForIngredientErrors(recipeObject);
+                });
         }
     }, [recipeObject, urlForPost])
+
+    const checkForIngredientErrors = () => {
+        let errorTicker = false;
+        recipeObject.ingredients.forEach((ingredient) => {
+
+            if (ingredient.item === '') {
+                errorTicker = true;
+
+                let duplicateIngredients = error;
+                duplicateIngredients.ingredient = <span style={{color: 'red'}}>Please delete any empty fields</span>;
+                setError(duplicateIngredients);
+
+            } else {
+                if (!errorTicker === true) {
+                    let duplicateIngredients = error;
+                    duplicateIngredients.ingredient = null;
+                    setError(duplicateIngredients);
+                }
+            }
+
+            if (ingredient.measurement === '') {
+                errorTicker = true;
+                //setIngredientError(<span style={{color: 'red'}}>Please delete any empty fields</span>);
+            } else {
+                if (!errorTicker === true) {
+                    //setIngredientError(null);
+                }
+            }
+        })
+    }
 
     return {data, error, setUrlForPost, setRecipeObject};
 }
